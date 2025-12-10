2c6597959f24c5d971ea76a91fc761460d6ee7b3
shukla-signity <s.jyotishwar@signitysolutions.com>
2025-12-10 12:38:40 +0530

    first commit

---
+++ map.js
@@ -0,0 +1,16 @@
+let myMap = new Map();
+myMap.set("city1","mohali");
+myMap.set("city2","Pune");
+myMap.set("city3","bengaluru");
+// myMap.delete("city2");
+// console.log(myMap.has("city2"));
+// myMap.set('name', 'Alice');
+// myMap.set(1, 'One');
+// myMap.set({}, 'An object key');
+// console.log(myMap.get('name')); // Output: Alice
+// console.log(myMap.has(1));      // Output: true
+// console.log(myMap.size);        // Output: 3
+// let arr = [1,3,4,5,6,7,8];
+// for (let mem of arr){
+// console.log(mem);
+// }
